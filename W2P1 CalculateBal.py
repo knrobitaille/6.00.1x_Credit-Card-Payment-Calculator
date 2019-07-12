3189562cd496e708699f3f8ab0a84415cf234251
krobitaille93 <52327814+krobitaille93@users.noreply.github.com>
2019-07-12 12:12:35 -0400

    Add files via upload

---
+++ W2P1 CalculateBal.py
@@ -0,0 +1,58 @@
+"""
+"""
+
+print("Welcome to the credit card payment calculator. Based on your balance, interest rate, and payment pattern, this calculator will show you if you will pay off your balance in a year and how much interest you will have paid.")
+
+balance = float(input("Enter your credit card balance: "))
+
+annualInterestRate = float(input("Enter your credit card interest rate: "))
+monthlyInterestRate = annualInterestRate/12
+
+while True:
+    paymentType = input("Do you plan to pay a fixed or variable amount? Please enter 'F' or 'V : ")
+    if paymentType in ["F","f","V","v"]:
+        break
+
+if paymentType == "V" or paymentType == "v":    
+    monthlyPaymentRate = float(input("Enter what percent of your balance you plan to pay each month: "))
+    
+    month = 0
+    b = balance
+    paid = 0
+
+    while month < 12:
+        p = b * monthlyPaymentRate
+        ub = b - p
+        b = ub * monthlyInterestRate + ub 
+        paid += p
+        month +=1
+        print("Month " + str(month) + " Remaining balance:" + str(round(b,2)))
+    if round(b,2) <= 0:
+        print("You paid off your balance and paid " + str(round(paid-balance,2)) + " in interest.")
+    else:
+        print("You did not pay off the whole balance.")
+    
+elif paymentType == "F" or paymentType == "f":   
+    monthlyPayment = float(input("Enter what fixed amount you plan to pay each month: "))
+
+    month = 0
+    b = balance
+    paid = 0
+    
+    while month < 12:
+        if b < monthlyPayment:
+            p = b
+        else:
+            p =  monthlyPayment
+        ub = b - p
+        b = ub * monthlyInterestRate + ub 
+        paid += p
+        month +=1
+        print("Month " + str(month) + " Remaining balance:" + str(max(round(b,2),0)))
+    if b <= 0:
+        print("You paid off your balance and paid " + str(round(paid-balance,2)) + " in interest.")
+    else:
+        print("You did not pay off the whole balance.")
+else:
+    print("Sorry, something went wrong.")
+
